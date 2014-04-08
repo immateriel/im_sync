967db66ce886373019e9aaeb20b1332cdeaa5cf5
julbouln <jboulnois@immateriel.fr>
2014-04-08 10:21:43 +0200

    onix2imml

--- lib/onix2imml.rb
+++ lib/onix2imml.rb
@@ -17,69 +17,79 @@ class Onix2Imml
 
         imml_book=IMML::Book::Book.create(product.ean)
 
-      imml_book.metadata=IMML::Book::Metadata.create(product.title,product.language_code_of_text,product.raw_description,product.subtitle,product.publication_date)
-      imml_book.metadata.publisher=IMML::Book::Publisher.create(product.publisher_name)
+        imml_book.metadata=IMML::Book::Metadata.create(product.title, product.language_code_of_text, product.raw_description, product.subtitle, product.publication_date)
+        imml_book.metadata.publisher=IMML::Book::Publisher.create(product.publisher_name)
 
-      product.contributors.each do |c|
-        imml_book.metadata.contributors << IMML::Book::Contributor.create(c.name,c.role.human.downcase)
-      end
-
-      imml_book.metadata.topics=IMML::Book::Topics.create
-
-      product.bisac_categories_codes.each do |bcc|
-        imml_book.metadata.topics << IMML::Book::Topic.create("bisac",bcc)
-      end
-
-      product.clil_categories_codes.each do |bcc|
-        imml_book.metadata.topics << IMML::Book::Topic.create("clil",bcc)
-      end
-
-      product.keywords.each do |bcc|
-        imml_book.metadata.topics << IMML::Book::Topic.create("keyword",bcc)
-      end
-
-      if product.publisher_collection_title
-        imml_book.metadata.collection=IMML::Book::Collection.create(product.publisher_collection_title)
-      end
-
-
-      imml_book.assets=IMML::Book::Assets.create
-      if product.frontcover_url
-        mimetype=product.frontcover_mimetype
-        unless mimetype
-          case product.frontcover_url
-            when /\.png$/
-              mimetype="image/png"
-            when /\.jpe?g$/
-              mimetype="image/jpeg"
-            when /\.gif$/
-              mimetype="image/gif"
+        product.contributors.each do |c|
+          role="author"
+          case c.role.human
+            when /Translated/
+              role="translator"
+            when /Edited/
+              role="editor"
+            when /Illustraded/
+              role="illustrator"
+            when /Photograph/
+              role="photographer"
           end
+          imml_book.metadata.contributors << IMML::Book::Contributor.create(c.name, role)
         end
-        imml_book.assets.cover=IMML::Book::Cover.create(mimetype,nil,product.frontcover_last_updated,nil,product.frontcover_url)
-      end
 
-      if product.epub_sample_url
-        imml_book.assets.extracts << IMML::Book::Extract.create(product.epub_sample_mimetype,nil,product.epub_sample_last_updated,product.epub_sample_url)
-      end
+        imml_book.metadata.topics=IMML::Book::Topics.create
 
-      if product.digital?
-        if product.bundle?
-          product.parts.each do |part|
-            if part.file_mimetype
-              imml_book.assets.fulls << IMML::Book::Full.create(part.file_mimetype,part.filesize)
+        product.bisac_categories_codes.each do |bcc|
+          imml_book.metadata.topics << IMML::Book::Topic.create("bisac", bcc)
+        end
+
+        product.clil_categories_codes.each do |bcc|
+          imml_book.metadata.topics << IMML::Book::Topic.create("clil", bcc)
+        end
+
+        product.keywords.each do |bcc|
+          imml_book.metadata.topics << IMML::Book::Topic.create("keyword", bcc)
+        end
+
+        if product.publisher_collection_title
+          imml_book.metadata.collection=IMML::Book::Collection.create(product.publisher_collection_title)
+        end
+
+        imml_book.assets=IMML::Book::Assets.create
+        if product.frontcover_url
+          mimetype=product.frontcover_mimetype
+          unless mimetype
+            case product.frontcover_url
+              when /\.png$/
+                mimetype="image/png"
+              when /\.jpe?g$/
+                mimetype="image/jpeg"
+              when /\.gif$/
+                mimetype="image/gif"
             end
           end
-        else
-          imml_book.assets.fulls << IMML::Book::Full.create(product.file_mimetype,product.filesize)
+          imml_book.assets.cover=IMML::Book::Cover.create(mimetype, nil, product.frontcover_last_updated, nil, product.frontcover_url)
         end
 
-      end
+        if product.epub_sample_url
+          imml_book.assets.extracts << IMML::Book::Extract.create(product.epub_sample_mimetype, nil, product.epub_sample_last_updated, product.epub_sample_url)
+        end
+
+        if product.digital?
+          if product.bundle?
+            product.parts.each do |part|
+              if part.file_mimetype
+                imml_book.assets.fulls << IMML::Book::Full.create(part.file_mimetype, part.filesize)
+              end
+            end
+          else
+            imml_book.assets.fulls << IMML::Book::Full.create(product.file_mimetype, product.filesize)
+          end
+
+        end
 
 #  pp product.supplies
 
 
-        imml_book.offer=IMML::Book::Offer.create("digital",product.available?)
+        imml_book.offer=IMML::Book::Offer.create("digital", product.available?)
         if product.pages
           imml_book.offer.pagination=product.pages
         end
@@ -91,12 +101,12 @@ class Onix2Imml
 
           if supply[:available]
             current_price=product.current_price_amount_for(supply[:currency]).to_f/100.0
-            imml_book.offer.prices << IMML::Book::Price.create(supply[:currency],current_price,supply[:territory].join(" "))
+            imml_book.offer.prices << IMML::Book::Price.create(supply[:currency], current_price, supply[:territory].join(" "))
           end
         end
 
         if product.print_product
-          imml_book.offer.alternatives << IMML::Book::Alternative.create(product.print_product.ean,"printed")
+          imml_book.offer.alternatives << IMML::Book::Alternative.create(product.print_product.ean, "printed")
         end
 
         @imml_books << imml_book
