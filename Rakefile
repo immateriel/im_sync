d9e5dc23db3a9fcba6f8df0d3faf5a7528b4f8bf
julbouln <jboulnois@immateriel.fr>
2014-04-08 08:27:48 +0200

    gem

---
+++ Rakefile
@@ -0,0 +1,39 @@
+# -*- encoding : utf-8 -*-
+begin
+  require 'jeweler'
+  Jeweler::Tasks.new do |gem|
+    gem.name = "im_sync"
+    gem.summary = %Q{immatériel.fr sync tools}
+    gem.description = %Q{immatériel.fr sync tools}
+    gem.email = "jboulnois@immateriel.fr"
+    gem.homepage = "http://github.com/immateriel/im_sync"
+    gem.authors = ["julbouln"]
+    gem.files = Dir.glob('bin/**/*') + Dir.glob('lib/**/*')
+    gem.add_dependency "imml"
+
+  end
+  Jeweler::GemcutterTasks.new
+
+  require 'rake/testtask'
+  Rake::TestTask.new(:test) do |test|
+    test.libs << 'lib' << 'test'
+    test.pattern = 'test/**/test_*.rb'
+    test.verbose = true
+  end
+
+  require 'rcov/rcovtask'
+  Rcov::RcovTask.new do |test|
+    test.libs << 'test'
+    test.pattern = 'test/**/test_*.rb'
+    test.verbose = true
+    test.rcov_opts << '--exclude "gems/*"'
+  end
+
+  task :default => :test
+
+
+
+rescue LoadError
+  puts "Jeweler (or a dependency) not available. Install it with: gem install jeweler"
+end
+
---
+++ lib/ask.rb
@@ -0,0 +1,43 @@
+
+class Ask
+  attr_accessor :doc
+
+#  @@ask_check_url="http://localhost:3000/fr/web_service/ask_check"
+#  @@ask_push_url="http://localhost:3000/fr/web_service/ask_push"
+
+  @@ask_check_url="https://ws.immateriel.fr/fr/web_service/ask_check"
+  @@ask_push_url="https://ws.immateriel.fr/fr/web_service/ask_push"
+
+  def initialize(receive_url,check_url)
+    @doc=IMML::Document.new
+    @doc.header=IMML::Header::Header.create
+    @doc.header.test=IMML::Header::Test.create(receive_url,check_url,nil)
+    nil
+  end
+
+  def add_book_param(ean)
+    @doc.header.params << IMML::Header::Param.create("book",ean)
+  end
+
+  def post(url)
+    uri = URI.parse(url)
+    http = Net::HTTP.new(uri.host, uri.port)
+    http.use_ssl = true
+    request = Net::HTTP::Post.new(uri.request_uri)
+    request.body = @doc.xml_builder.to_xml
+    response = http.request(request)
+    if response.code.to_i/200 == 1
+      true
+    else
+      false
+    end
+  end
+
+  def ask_check
+    self.post(@@ask_check_url)
+  end
+  def ask_push
+    self.post(@@ask_push_url)
+  end
+
+end
